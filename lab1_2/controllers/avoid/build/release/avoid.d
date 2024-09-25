777e407d2e1a92abd55daef09366cd305f24c6d1
Nirbhay Raut <117532289+sorashiro3@users.noreply.github.com>
2024-09-25 13:23:21 +0530

    Add files via upload

---
+++ lab1_2/controllers/avoid/build/release/avoid.d
@@ -0,0 +1,6 @@
+build/release/avoid.o: avoid.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp
